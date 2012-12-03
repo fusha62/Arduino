ca5522d0cff420efb93b2eca052fc83070e2a5fc
Tomohiro Minagawa <fusha01@hotmail.com>
2012-12-03 22:24:44 +0900

    タッチスクリーンにおけるタッチ位置を，シリアルで転送するようなコードを組み込んだ．

--- TouchShield/TouchCircle/TouchCircle.ino
+++ TouchShield/TouchCircle/TouchCircle.ino
@@ -16,9 +16,12 @@
   #define XP 57   // can be a digital pin, this is A3 
 #endif
 
+// Touch Screen Init
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+
 void setup()
 {
-
+  Serial.begin(9600); // Serial Init
   Tft.init(); // init TFT library
   
   Tft.drawString(" Please Write Circle ...",0,280,1,WHITE); 
@@ -26,5 +29,13 @@ void setup()
 
 void loop()
 {
+  // a point object holds x y and z coordinates
+  Point p = ts.getPoint();
+  
+  if (p.z > ts.pressureThreshhold) {
+     Serial.print("Raw X = "); Serial.print(p.x);
+     Serial.print("\tRaw Y = "); Serial.print(p.y);
+     Serial.print("\tPressure = "); Serial.println(p.z);
+  }
   
 }
