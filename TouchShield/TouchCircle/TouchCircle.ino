2b9e721a335b8d3cd1956840c7608c8946345b52
Tomohiro Minagawa <fusha01@hotmail.com>
2012-12-04 15:57:21 +0900

    円を描くと（正確には４分割したタッチスクリーンを正規の手順で押すと）．Circleと表示されるようにした．

--- TouchShield/TouchCircle/TouchCircle.ino
+++ TouchShield/TouchCircle/TouchCircle.ino
@@ -19,6 +19,9 @@
 // Touch Screen Init
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 
+// Global Variance
+int fl=0; // Touch Status
+
 void setup()
 {
   Serial.begin(9600); // Serial Init
@@ -31,36 +34,56 @@ void loop()
 {
   // a point object holds x y and z coordinates
   Point p = ts.getPoint();
-  int fl;
-  fl = 0;
   
   if (p.z > ts.pressureThreshhold) {
      Serial.print("Raw X = "); Serial.print(p.x);
      Serial.print("\tRaw Y = "); Serial.print(p.y);
      Serial.print("\tPressure = "); Serial.println(p.z);
+     Serial.print("\tfl = "); Serial.println(fl);
      
      if ((p.x > 500) && (p.y < 500)) {
-       Tft.drawString("Point 1 Touch OK !",0,100,1,WHITE);
-       delay(1000);
-       setup();
+       // Point 1 Touched
+       if ((fl == 0) || (fl == 1)){
+         fl = 1;
+       }else{
+         fl = 0;
+       }
      }
      
      if ((p.x > 500) && (p.y > 500)){
-       Tft.drawString("Point 2 Touch OK !",0,200,1,WHITE);
-       delay(1000);
-       setup();
+       // Point 2 Touched
+       if ((fl == 1) || (fl == 3)){
+         fl = 3;
+       }else{
+         fl = 0;
+       }
      }
      
      if ((p.x < 500) && (p.y > 500)){
-       Tft.drawString("Point 3 Touch OK !",0,200,1,WHITE);
-       delay(1000);
-       setup();
+       // Point 3 Touched
+       if ((fl == 3) || (fl == 7)){
+         fl = 7;
+       }else{
+         fl = 0;
+       }
      }
      
      if ((p.x < 500) && (p.y < 500)) {
-       Tft.drawString("Point 4 Touch OK !",0,100,1,WHITE);
-       delay(1000);
-       setup();
+       // Point 4 Touched
+       if ((fl == 7) || (fl == 15)){
+         fl = 15;
+         delay(500);
+       }else{
+         fl = 0;
+       }
      }
+  }
+  
+  if (fl == 15){
+    // Draw Circle Completed!
+    Tft.drawString("Circle!!",0,100,2,WHITE);
+    delay(1000);
+    fl = 0;
+    setup();
   }
 }
